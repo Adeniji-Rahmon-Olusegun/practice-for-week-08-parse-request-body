8384bad08ba062db4a7b291433cae20e3d0297c0
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-02-02 21:37:18 +0100

    parse-request-body

--- parse-body.js
+++ parse-body.js
@@ -1,26 +1,75 @@
 function firstStep(input) {
   // Your code here
+  let output = input.split("&");
+
+  return output;
 }
 
 function secondStep(input) {
   // Your code here
+  //let strArr = firstStep(input);
+
+  let twoDArr = input.map((ele) => {
+    return ele.split("=");
+  });
+
+  return twoDArr;
 }
 
-function thirdStep(input) {
+function thirdStep(arr2) {
   // Your code here
+  for (let i = 0; i < arr2.length; i++) {
+    let item = arr2[i];
+    for (let j = 0; j < item.length; j++) {
+        let subItem = item[j];
+
+        if (subItem.includes("+")) {
+            arr2[i][j] = subItem.replace("+", " ");
+        }
+    }
+  }
+
+  return arr2;
 }
 
-function fourthStep(input) {
+function fourthStep(arr2) {
   // Your code here
+  for (let i = 0; i < arr2.length; i++) {
+    let item = arr2[i];
+    for (let j = 0; j < item.length; j++) {
+        let subItem = item[j];
+
+        if (subItem.includes("%")) {
+            arr2[i][j] = decodeURIComponent(subItem);
+        }
+    }
+  }
+
+  return arr2;
 }
 
-function fifthStep(input) {
+function fifthStep(arr2) {
   // Your code here
+
+  return Object.fromEntries(arr2);
+
 }
 
 function parseBody(str) {
   // Your code here
+  let arr1 = firstStep(str);
+  let arr2 = secondStep(arr1);
+  let arr3 = thirdStep(arr2);
+  let arr4 = fourthStep(arr3);
+
+  let obj = fifthStep(arr4);
+
+  return obj;
 }
+
+let str = "name=Fido&color=black&age=1&description=Hello+World%21";
+
+//console.log(secondStep(str));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
+const http = require('http');
+
 let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+server = http.createServer((req, res) => {
+
+    let streamData = '';
+
+    console.log(req.method, req.url);
+
+    req.on('data', (data) => {
+        streamData += data;
+    });
+
+    req.on('end', () => {
+        req.body = parseBody(streamData);
+        sendFormPage(req, res);
+    });
+    
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log("Successfully started the server on port", port));
+
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
